8cae38fb6cad7e488494b10173945db063625480
Chrisclrk9 <chrisclrk9@gmail.com>
2018-03-15 08:28:19 +0000

    messing about with functions scope

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-function test() {
-  alert(" Who ae all the pies?");
-        
+function pies() {
+    
+    alert(" Who ae all the pies?");
+
+
+function test(){
     document.getElementById("demo").innerHTML = "Who ate all the pies";
     document.getElementById("blue").style.backgroundColor = "yellow";
     
-    };
+    return test;
+    
+};
+};
