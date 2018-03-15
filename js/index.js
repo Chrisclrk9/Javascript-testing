293591e58f1a8621f7774d20f94da7b6a9f82f6d
Chrisclrk9 <chrisclrk9@gmail.com>
2018-03-15 09:35:26 +0000

    test

--- js/index.js
+++ js/index.js
@@ -2,12 +2,7 @@ function pies() {
     
     alert(" Who ae all the pies?");
 
-
-function test(){
     document.getElementById("demo").innerHTML = "Who ate all the pies";
     document.getElementById("blue").style.backgroundColor = "yellow";
-    
-    return test;
-    
-};
+        
 };
