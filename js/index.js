4d79a78845bc31c2a8b905af35adb3f42cc56923
Chrisclrk9 <chrisclrk9@gmail.com>
2018-03-15 00:11:15 +0000

    onclick tested
    using js in varous way to test out my knowledge

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function test() {
+  alert(" Who ae all the pies?");
+        
+    document.getElementById("demo").innerHTML = "Who ate all the pies";
+    document.getElementById("blue").style.backgroundColor = "yellow";
+    
+    };
