14c3ba11a2994778a662edd96c76bd2493923516
Chrisclrk9 <chrisclrk9@gmail.com>
2018-03-15 10:27:32 +0000

    Hidden display button

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
-function pies() {
-    
-    alert(" Who ae all the pies?");
+//function pies() {
+//    
+//    alert(" Who ae all the pies?");
+//
+//    document.getElementById("demo").innerHTML = "Who ate all the pies";
+//    document.getElementById("blue").style.backgroundColor = "yellow";
+//        
+//};
 
-    document.getElementById("demo").innerHTML = "Who ate all the pies";
-    document.getElementById("blue").style.backgroundColor = "yellow";
+
+
+function hiddenText(){
+    
+   var x = document.getElementById('hide');
+    if(x.style.display == "none"){
+        x.style.display = "block";
+    }else{
         
-};
+    x.style.display = "none";
+    }
+    
+    
+}
